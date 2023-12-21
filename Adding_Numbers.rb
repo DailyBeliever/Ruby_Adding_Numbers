a74dc89668cd9bf0ba301376c53343673abd36b4
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2023-12-20 22:02:13 -0800

    First

---
+++ Adding_Numbers.rb
@@ -0,0 +1,6 @@
+# This is my first Ruby program
+
+name = "Daily"
+
+puts"Hello Everyone!"
+puts("My name is: " + name)
---
+++ Log.md
@@ -0,0 +1,5 @@
+# Updates:
+
+### 12/20/23
+- Created Repo
+- Added basic code
