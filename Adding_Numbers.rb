73784de6d57fad2d1f132e7f06a19fdbcac2d5a3
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2023-12-25 20:51:03 -0800

    User Input

--- Adding_Numbers.rb
+++ Adding_Numbers.rb
@@ -1,10 +1,20 @@
 # This is my first Ruby program
 
+# Variable "name" is holding Daily
 name = "Daily"
 
+# prints out with newline..
 puts"Hello Everyone!"
 puts("My name is: " + name)
 
-number1 = 1
+print("What is your name? ")
+# stores user input into "input" variable
+input = gets.chomp()
 
-number2 = 2
+# prints out string with input
+print ("Hello " + input + "! Welcome to the Adding Numbers Program!")
+
+
+
+# number1 = 1
+# number2 = 2
--- Log.md
+++ Log.md
@@ -3,3 +3,6 @@
 ### 12/20/23
 - Created Repo
 - Added basic code
+
+### 12/25/23
+- Added User Input
